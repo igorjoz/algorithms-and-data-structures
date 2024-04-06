265a77401c0010f0f8258fe37fd9588e9395a9e8
Igor Józefowicz <igorjozefowicz@gmail.com>
2024-04-06 23:34:29 +0200

    add Merge Sort algorithm

---
+++ algorithms/sorting algorithms/Merge Sort.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+
+using namespace std;
+
+
+// Merges two subarrays of arr[].
+// 1. subarray is arr[l..m]
+// 2. subarray is arr[m+1..r]
+void merge(int arr[], int l, int m, int r)
+{
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    int* L = new int[n1];
+    int* R = new int[n2];
+
+    for (int i = 0; i < n1; i++) {
+        L[i] = arr[l + i];
+    }
+    for (int j = 0; j < n2; j++) {
+        R[j] = arr[m + 1 + j];
+    }
+
+    int i = 0; // index of first subarray
+    int j = 0; // index of second subarray
+    int k = l; // index of merged subarray
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+// l - left index, and r - right index of the sub-array
+void mergeSort(int arr[], int l, int r) {
+    if (l >= r) {
+        return; //returns recursively
+    }
+    int m = l + (r - l) / 2;
+    mergeSort(arr, l, m);
+    mergeSort(arr, m + 1, r);
+    merge(arr, l, m, r);
+}
+
+
+void printArray(int A[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << A[i] << " ";
+    }
+}
+
+
+int main()
+{
+    int arr[] = { 12, 11, 13, 5, 6, 7 };
+    int arr_size = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Given array is \n";
+    printArray(arr, arr_size);
+
+    mergeSort(arr, 0, arr_size - 1);
+
+    cout << "\nSorted array is \n";
+    printArray(arr, arr_size);
+}
